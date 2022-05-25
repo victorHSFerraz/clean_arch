6e53dbaab81e13095f781de2d24a7141d2904551
victorHSFerraz <victorf178@gmail.com>
2022-05-24 21:38:49 -0300

    model toJson/fromJson & fixtures

---
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:clean_tdd_flutter/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:clean_tdd_flutter/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  const tNumberTriviaModel = NumberTriviaModel(
+    text: "text",
+    number: 1,
+  );
+
+  test(
+    'should be a subclass of NumberTrivia entity',
+    () async {
+      // arrange
+      expect(tNumberTriviaModel, isA<NumberTrivia>());
+    },
+  );
+
+  group('fromJson', () {
+    test(
+      'should return a valid model when the JSON number is an integer',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+    test(
+      'should return a valid model when the JSON number is regarded as a double ',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap = json.decode(fixture('trivia_double.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+  });
+
+  group('toJson', () {
+    test(
+      'should return a JSON map containing the proper data',
+      () async {
+        // act
+        final result = tNumberTriviaModel.toJson();
+        // assert
+        final expectedJsonMap = {
+          "text": "text",
+          "number": 1,
+        };
+        expect(result, expectedJsonMap);
+      },
+    );
+  });
+}
